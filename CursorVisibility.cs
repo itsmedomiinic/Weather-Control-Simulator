dabbcf286ae4f366d633fb06b62c89be423f6c1c
Dominic Vega Ocampo <73176566+itsmedomiinic@users.noreply.github.com>
2022-01-31 18:26:22 +0000

    C# Files

---
+++ CursorVisibility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class CursorVisibility : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+    }     
+}
